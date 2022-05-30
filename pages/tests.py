f36ddf01617e551b7b738490edeaaa5c50ca6a61
Vicky W <alwaysvicky23@gmail.com>
2022-05-30 18:31:07 -0400

    class 1 chgs

--- pages/tests.py
+++ pages/tests.py
@@ -19,15 +19,15 @@ class SimplePageTests(SimpleTestCase):
     def test_home_page_reverse_lookup(self):
         response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "index.html"
+        self.assertTemplateUsed(response, "index.html")
 
     def test_home_page_reverse_lookup(self):
         response = self.client.get(reverse("about"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, "about.html"
+        self.assertTemplateUsed(response, "about.html")
         
-    def test_about_page_contains_correct_content(self)
-        respone = self.client.get(reverse)"home"))
+    def test_about_page_contains_correct_content(self):
+        respone = self.client.get(reverse("home"))
         self.assertContains(response, "My Django App")
        
     def test_about_page_contains_correct_content(self):
